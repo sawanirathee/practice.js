6d5efa38c564588d5b11ded27aa0150684100d8d
sawanirathee <sawanirathee95@gmail.com>
2025-09-13 11:27:05 +0530

    how to declare variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+//how to create variables in js 
+
+const accountId = 145678; /* const ko ham reassinge or redecalare nhi krr sakte
+const accountId = 123455 // not allowed
+accountId = 34567 //not allowed */
+
+let Name = "shivani" /* let ko ham redeclare nhi krr sakte reassinge krr sakte hai
+let Name = "Nidhi" //not allowed
+Name = "Nidhi" //allowed */
+
+var accountPassword = "345678" /* var ko ham reassinge krr sakte hai or 
+same name se redeclare bhi krr sakte hai
+var accountPassword = "809876" //allowed
+accountPassword = "8790576" //allowed */
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope 
+*/
